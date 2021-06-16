6688f4e531125d2e925469f71cf50e7f4ec7bc21
georgevio <georgevio@gmail.com>
2021-06-17 02:24:47 +0300

    test started

--- examples/ipv6/rpl-udp-g/udp-client-test.c
+++ examples/ipv6/rpl-udp-g/udp-client-test.c
@@ -47,10 +47,10 @@ static uip_ipaddr_t server_ipaddr;
 static uip_ipaddr_t destination_ipaddr;
 
 /* Get the preffered parent, and the current own IP of the node */
-#include "net/rpl/rpl-icmp6.c"
-extern   rpl_parent_t *dao_preffered_parent;
-extern   uip_ipaddr_t *dao_preffered_parent_ip;
-extern   uip_ipaddr_t dao_prefix_own_ip;
+//#include "net/rpl/rpl-icmp6.c"
+//extern   rpl_parent_t *dao_preffered_parent;
+//extern   uip_ipaddr_t *dao_preffered_parent_ip;
+//extern   uip_ipaddr_t dao_prefix_own_ip;
 
 /* Monitor this var. When changed, the node has changed parent */
 static rpl_parent_t *my_cur_parent;
