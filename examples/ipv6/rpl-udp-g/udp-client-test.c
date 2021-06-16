121eeafb457491a304e98ae65b7663a86de78d9e
georgevio <georgevio@gmail.com>
2021-06-17 02:30:32 +0300

    test started

--- examples/ipv6/rpl-udp-g/udp-client-test.c
+++ examples/ipv6/rpl-udp-g/udp-client-test.c
@@ -42,8 +42,39 @@
 
 static struct uip_udp_conn *client_conn;
 static uip_ipaddr_t server_ipaddr;
+static uip_ipaddr_t destination_ipaddr;
 
+/* Get the preffered parent, and the current own IP of the node */
+//#include "net/rpl/rpl-icmp6.c"
+//extern   rpl_parent_t *dao_preffered_parent;
+//extern   uip_ipaddr_t *dao_preffered_parent_ip;
+//extern   uip_ipaddr_t dao_prefix_own_ip;
 
+/* Monitor this var. When changed, the node has changed parent */
+//static rpl_parent_t *my_cur_parent;
+//static uip_ipaddr_t *my_cur_parent_ip;
+
+static int counter=0; //counting rounds. Not really needed
+
+/* When this variable is true, start sending UDP stats */
+static uint8_t sendUDP = 0; 
+
+/* When this variable is true, start sending ICMP stats */
+static uint8_t sendICMP = 0; 
+
+/* When true, the controller will start probing all nodes for detais */
+static int enablePanicButton = 0;
+
+/* When the controller detects version number attack, it orders to stop
+ * resetting the tricle timer. The variables below lie in rpl-dag.c
+ */
+//#include "net/rpl/rpl-dag.c"
+//extern uint8_t ignore_version_number_incos; //if == 1 DIO will not reset trickle
+//extern uint8_t dio_bigger_than_dag; // if version attack, this will be 1
+//extern uint8_t dio_smaller_than_dag; // if version attack, this will be 1
+ 
+static uint8_t prevICMRecv = 0;
+static uint8_t prevICMPSent = 0;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(udp_client_process, "UDP client process");
