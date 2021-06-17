d198d58396e746f95860ebd867536a2b9aa028b4
georgevio <georgevio@gmail.com>
2021-06-17 10:05:39 +0300

    test started

--- examples/ipv6/rpl-udp-g/udp-client-small.c
+++ examples/ipv6/rpl-udp-g/udp-client-small.c
@@ -4,36 +4,17 @@
  */
 
 #include "contiki.h"
-#include "net/ip/uip.h"
+//#include "net/ip/uip.h"
 
 //#include "dixonQtestIn.c"
 //#include "dixonQtestOut.c"
 
-#define UDP_CLIENT_PORT 8765
-#define UDP_SERVER_PORT 5678
 
-#define UDP_EXAMPLE_ID  190
-
-#ifndef PERIOD
-#define PERIOD 500 /* increase it to 700 avoid flooding */
-//#define PERIOD 60
-#endif
-
-#define START_INTERVAL		(15 * CLOCK_SECOND)
-#define SEND_INTERVAL		(PERIOD * CLOCK_SECOND)
-#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
-#define MAX_PAYLOAD_LEN		30
-
-static struct uip_udp_conn *client_conn;
-static uip_ipaddr_t server_ipaddr;
-static uip_ipaddr_t destination_ipaddr;
-
-/* Get the preffered parent, and the current own IP of the node */
+//static struct uip_udp_conn *client_conn;
+//static uip_ipaddr_t server_ipaddr;
+//static uip_ipaddr_t destination_ipaddr;
 
 
-
-// ERROR IS HERE !!!!!!!!!!!!!!!!
-// even when i include above the rpl-icmp6, it gives errr????/
 //#include "core/net/rpl/rpl-icmp6.c"
 
 
@@ -45,7 +26,7 @@ static uip_ipaddr_t destination_ipaddr;
 /* Monitor this var. When changed, the node has changed parent */
 //static rpl_parent_t *my_cur_parent;
 
-static uip_ipaddr_t *my_cur_parent_ip;
+//static uip_ipaddr_t *my_cur_parent_ip;
 
 
 
@@ -74,11 +55,6 @@ PROCESS_THREAD(udp_client_process, ev, data)
 
   while(1) {
     PROCESS_YIELD();
-
-
-    if(data != NULL) {
-
-    }
 
 
   }
